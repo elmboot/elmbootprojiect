e79d4021372c82e9c7c0c6bbb624507e21f27af3
bocai <yinbocai@163.com>
2020-08-04 16:51:11 +0800

    elm项目初始化

---
+++ src/main/java/com/neusoft/elmboot/service/impl/UserServiceImpl.java
@@ -0,0 +1,25 @@
+package com.neusoft.elmboot.service.impl;
+
+import com.neusoft.elmboot.mapper.UserMapper;
+import com.neusoft.elmboot.po.User;
+import com.neusoft.elmboot.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+    @Override
+    public List<User> selectUserList() {
+        try{
+            return userMapper.selectUserList();
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
