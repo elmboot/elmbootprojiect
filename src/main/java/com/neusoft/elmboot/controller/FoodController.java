70a47353f8de54e5ffaf01af7944fbfec62cdd1b
bocai <yinbocai@163.com>
2020-08-06 15:48:41 +0800

    食品模块

--- src/main/java/com/neusoft/elmboot/controller/FoodController.java
+++ src/main/java/com/neusoft/elmboot/controller/FoodController.java
@@ -1,7 +1,9 @@
 package com.neusoft.elmboot.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/food")
 public class FoodController {
 }
